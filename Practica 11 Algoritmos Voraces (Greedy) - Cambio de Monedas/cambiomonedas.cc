c5fe4d3e933d142bc54c31e3b909f29eadc2d9a2
feichay10 <alu0101364544@ull.edu.es>
2022-12-14 10:07:53 +0000

    Empezamos p11

---
+++ Practica 11 Algoritmos Voraces (Greedy) - Cambio de Monedas/cambiomonedas.cc
@@ -0,0 +1,28 @@
+/**
+ *
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Asignatura: Computabilidad y Algoritmia
+ * Curso: 2º
+ * Práctica 11: Algoritmos Voraces (Greedy) - Cambio de Monedas
+ * @file cambiomonedas.cc
+ * @author Cheuk Kelly Ng Pante (alu0101364544@ull.edu.es)
+ * @brief Programa que calcula el cambio de monedas de una cantidad dada
+ * @version 0.1
+ * @date 2022-11-22
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include "monedero.h"
+
+#include <iostream>
+
+int main () {
+  float n;
+
+  std::cout << "Introduzca la cantidad a cambiar: ";
+  std::cin >> n;
+}
---
+++ Practica 11 Algoritmos Voraces (Greedy) - Cambio de Monedas/monedero.h
@@ -0,0 +1,25 @@
+/**
+ *
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Asignatura: Computabilidad y Algoritmia
+ * Curso: 2º
+ * Práctica 11: Algoritmos Voraces (Greedy) - Cambio de Monedas
+ * @file monedero.h
+ * @author Cheuk Kelly Ng Pante (alu0101364544@ull.edu.es)
+ * @brief
+ * @version 0.1
+ * @date 2022-11-22
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include <vector>
+#include <list>
+
+class Monedero {
+ private:
+ public:
+};
