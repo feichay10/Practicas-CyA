52244bafb63716ca4e4de793193e896c82c33f6b
Cheuk Kelly Ng Pante <alu0101364544@ull.edu.es>
2023-01-12 10:22:23 +0000

    Modificacion p12

---
+++ Practica 12 Algoritmos Divide y Vencerás (Divide & Conquer) -  Multiplicación de enteros grandes/Modificacion/bigint.h
@@ -0,0 +1,100 @@
+/**
+ *
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Asignatura: Computabilidad y Algoritmia
+ * Curso: 2º
+ * Práctica 12: Algoritmos Divide y Vencerás (Divide & Conquer) - Multiplicación
+ * de enteros grandes
+ * @file bigint.cc
+ * @author Cheuk Kelly Ng Pante (alu0101364544@ull.edu.es)
+ * @brief Definición de la clase BigInt y sus metodos
+ * @version 0.1
+ * @date 2023-01-10
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#pragma once
+
+#include <bits/stdc++.h>
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+
+class BigInt {
+ public:
+  // Constructors:
+  BigInt(unsigned long long n = 0);
+  BigInt(std::string&);
+  BigInt(const char *);
+  BigInt(const BigInt &);
+
+  // Helper Functions:
+  friend void divide_by_2(BigInt &a);
+  friend bool Null(const BigInt &);
+  friend int Length(const BigInt &);
+  int operator[](const int) const;
+
+  /* * * * Operator Overloading * * * */
+
+  // Direct assignment
+  BigInt &operator=(const BigInt &);
+
+  // Post/Pre - Incrementation
+  BigInt &operator++();
+  BigInt operator++(int temp);
+  BigInt &operator--();
+  BigInt operator--(int temp);
+
+  // Addition and Subtraction
+  friend BigInt &operator+=(BigInt &, const BigInt &);
+  friend BigInt operator+(const BigInt &, const BigInt &);
+  friend BigInt operator-(const BigInt &, const BigInt &);
+  friend BigInt &operator-=(BigInt &, const BigInt &);
+
+  // Comparison operators
+  friend bool operator==(const BigInt &, const BigInt &);
+  friend bool operator!=(const BigInt &, const BigInt &);
+
+  friend bool operator>(const BigInt &, const BigInt &);
+  friend bool operator>=(const BigInt &, const BigInt &);
+  friend bool operator<(const BigInt &, const BigInt &);
+  friend bool operator<=(const BigInt &, const BigInt &);
+
+  // Multiplication and Division
+  friend BigInt &operator*=(BigInt &, const BigInt &);
+  friend BigInt operator*(const BigInt &, const BigInt &);
+  friend BigInt &operator/=(BigInt &, const BigInt &);
+  friend BigInt operator/(const BigInt &, const BigInt &);
+
+  // Modulo
+  friend BigInt operator%(const BigInt &, const BigInt &);
+  friend BigInt &operator%=(BigInt &, const BigInt &);
+
+  // Power Function
+  friend BigInt &operator^=(BigInt &, const BigInt &);
+  friend BigInt operator^(BigInt &, const BigInt &);
+
+  // Square Root Function
+  friend BigInt sqrt(BigInt &a);
+
+  // Read and Write
+  friend std::ostream &operator<<(std::ostream &, const BigInt &);
+  friend std::istream &operator>>(std::istream &, BigInt &);
+
+  // Others
+  friend BigInt NthCatalan(int n);
+  friend BigInt NthFibonacci(int n);
+  friend BigInt Factorial(int n);
+
+  BigInt Karatsuba(BigInt num1, BigInt num2, int cota);
+
+ private:
+  std::string digits;
+};
---
+++ Practica 12 Algoritmos Divide y Vencerás (Divide & Conquer) -  Multiplicación de enteros grandes/Modificacion/multbigint.cc
@@ -0,0 +1,143 @@
+/**
+ *
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Asignatura: Computabilidad y Algoritmia
+ * Curso: 2º
+ * Práctica 12: Algoritmos Divide y Vencerás (Divide & Conquer) - Multiplicación
+ * de enteros grandes
+ * @file multbigint.cc
+ * @author Cheuk Kelly Ng Pante (alu0101364544@ull.edu.es)
+ * @brief
+ * @version 0.1
+ * @date 2023-01-10
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include <iostream>
+#include <chrono>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+
+#include "bigint.h"
+
+const std::string HELP1 = "-h";
+const std::string HELP2 = "--help";
+const std::string KARATSUBA = "-k";
+const std::string OPERATOR = "-m";
+const std::string COTA = "-n";
+const std::string RANDOM = "-r";
+
+void check_parameters(int argc, char* argv[]);
+
+int main(int argc, char* argv[]) {
+  BigInt karatsubaOperation;
+  BigInt result;
+  std::string number1, number2, line;
+  std::vector<std::string> aux;
+  
+  check_parameters(argc, argv);
+
+  if(argc == 1 || argv[1] == KARATSUBA) {
+    for (int i = 0; i < 2; ++i) {
+      getline(std::cin, line);
+      aux.push_back(line);
+    }
+
+    std::cout << "Primer numero = " << aux[0] << std::endl;
+    std::cout << "Segundo numero = " << aux[1] << std::endl;
+    BigInt num1(aux[0]);
+    BigInt num2(aux[1]);
+    auto t1 = std::chrono::high_resolution_clock::now();
+    result = karatsubaOperation.Karatsuba(num1, num2, 0);
+    auto t2 = std::chrono::high_resolution_clock::now();
+    auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count();
+    std::cout << "La solución de la operación con el algoritmo Karatsuba es: " << result << std::endl;
+    std::cout << "El tiempo de ejecución del algoritmo de Karatsuba: " << time << " nanosegundos" << std::endl;
+    exit(0);
+  } else if (argv[1] == OPERATOR) {
+    while(getline(std::cin, line)){
+      aux.push_back(line);
+    }
+
+    std::cout << "Primer numero = " << aux[0] << std::endl;
+    std::cout << "Segundo numero = " << aux[1] << std::endl;
+
+    BigInt num1(aux[0]); 
+    BigInt num2(aux[1]);
+    auto t1 = std::chrono::high_resolution_clock::now();
+    std::cout << "La solución de la operación con el operador * es: " << num1 * num2 << std::endl;
+    auto t2 = std::chrono::high_resolution_clock::now();
+    auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count();
+    std::cout << "El tiempo de ejecución del operador *: " << time << " nanosegundos" << std::endl;
+    exit(0);
+  } else if (argv[1] == COTA) {
+    while(getline(std::cin, line)){
+      aux.push_back(line);
+    }
+
+    std::cout << "Primer numero = " << aux[0] << std::endl;
+    std::cout << "Segundo numero = " << aux[1] << std::endl;
+
+    auto t1 = std::chrono::high_resolution_clock::now();
+    result = karatsubaOperation.Karatsuba(BigInt(aux[0]), BigInt(aux[1]), std::stoi(argv[2]));
+    auto t2 = std::chrono::high_resolution_clock::now();
+    auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count();
+    std::cout << "La solución de la operación con el algoritmo Karatsuba es: " << result << std::endl;
+    std::cout << "El tiempo de ejecución del algoritmo de Karatsuba: " << time << " nanosegundos" << std::endl;
+    exit(0);
+  } 
+  
+  if (argv[1] == RANDOM) {
+    srand(time(NULL));
+    int size = std::stoi(argv[2]);
+    for (int i = 0; i < size; i++) {
+      number1 += std::to_string(rand() % 10);
+      number2 += std::to_string(rand() % 10);
+    }
+    aux.push_back(number1);
+    aux.push_back(number2);
+
+    std::cout << "Primer numero aleatorio generado = " << aux[0] << std::endl;
+    std::cout << "Segundo numero aleatorio generado = " << aux[1] << std::endl;
+
+    auto t1 = std::chrono::high_resolution_clock::now();
+    result = karatsubaOperation.Karatsuba(BigInt(aux[0]), BigInt(aux[1]), 0);
+    auto t2 = std::chrono::high_resolution_clock::now();
+    auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count();
+    std::cout << "La solución de la operación con el algoritmo Karatsuba con numeros aleatorios es: " << result << std::endl;
+    std::cout << "El tiempo de ejecución del algoritmo de Karatsuba: " << time << " nanosegundos" << std::endl;
+    exit(0);
+  }
+  return 0;
+}
+
+void check_parameters(int argc, char* argv[]) {
+  if (argc == 2) {
+    std::string parameter = argv[1];
+    if (parameter == HELP1 || parameter == HELP2) {
+      std::cout << "Uso: " << argv[0] << " [OPCION]" << std::endl;
+      std::cout << "OPCION:" << std::endl;
+      std::cout << "  -h, --help\t\tAyuda del programa." << std::endl;
+      std::cout << "  -k\t\t\tUsa el algoritmo Karatsuba." << std::endl;
+      std::cout << "  -m\t\t\tUsa el operador * de la clase BigInt." << std::endl;
+      std::cout << "  -n [COTA]\t\tUsa el algoritmo Karatsuba con una cota." << std::endl;
+      std::cout << "  -r [TAMANO]\t\tUsa el algoritmo Karatsuba con numeros aleatorios." << std::endl;
+      exit(0);
+    } 
+    if (parameter != KARATSUBA && parameter != OPERATOR && parameter != COTA && parameter != RANDOM) {
+      std::cout << "Error: argumento no válido. Use la opcion -h o --help para saber el funcionamiento del programa." << std::endl;
+      exit(1);
+    }
+  } else if (argc == 4) {
+    std::cout << "Error: demasiados argumentos. Use la opcion -h o --help para saber el funcionamiento del programa." << std::endl;
+    exit(1);
+  } else if (argc == 1) {
+    std::cout << "Se usará el algoritmo Karatsuba por defecto." << std::endl;
+  }
+}
